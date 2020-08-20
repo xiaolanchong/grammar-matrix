f5ec6b9ffab114221e4a04e8ca8080fcdff2e564
Eugene Gorbachev <eugeneg@cqg.com>
2020-08-20 23:29:03 +0300

    Updates

---
+++ precache-manifest.1b1e2a4f4cc44a995fca77311a03c043.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0e1b7004514fc8e90e2c551e0461214",
+    "url": "/grammar-matrix/index.html"
+  },
+  {
+    "revision": "c5379cda19736bc019fd",
+    "url": "/grammar-matrix/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "505066c009b239156521",
+    "url": "/grammar-matrix/static/css/main.4678ec94.chunk.css"
+  },
+  {
+    "revision": "c5379cda19736bc019fd",
+    "url": "/grammar-matrix/static/js/2.77962b2c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/grammar-matrix/static/js/2.77962b2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "505066c009b239156521",
+    "url": "/grammar-matrix/static/js/main.ca03bf5f.chunk.js"
+  },
+  {
+    "revision": "bd1f903b1b96699eee65",
+    "url": "/grammar-matrix/static/js/runtime-main.084f23c6.js"
+  },
+  {
+    "revision": "e5ba4f95d934cd5d52f90b6abd26b5ea",
+    "url": "/grammar-matrix/static/media/Chevron2.e5ba4f95.svg"
+  },
+  {
+    "revision": "f9eaae25ef1764bdcd9d1634e9df8385",
+    "url": "/grammar-matrix/static/media/Chevron3.f9eaae25.svg"
+  }
+]);
